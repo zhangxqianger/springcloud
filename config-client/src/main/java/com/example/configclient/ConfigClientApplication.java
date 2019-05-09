e5fe4c2df800cf30b73641d523c808c9c5996395
zxq <zhangxiaoqiang@panda-fintech.com>
2019-05-09 18:33:38 +0800

    init

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,8 +2,14 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@RefreshScope
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
