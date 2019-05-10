c916c9b1ea959730e85fe5d76d3c18c126ff5be1
zxq <zhangxiaoqiang@panda-fintech.com>
2019-05-10 17:20:41 +0800

    init

--- serice-feign/src/main/java/com/example/sericefeign/SericeFeignApplication.java
+++ serice-feign/src/main/java/com/example/sericefeign/SericeFeignApplication.java
@@ -2,14 +2,20 @@ package com.example.sericefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@EnableHystrix
 public class SericeFeignApplication {
 
     public static void main(String[] args) {
